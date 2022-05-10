076e480918b13a55a6a9c38dce7ce09727a84668
charliedavidson <charliedavidson658@gmail.com>
2022-05-10 16:19:40 -0400

    Move to src

--- src/NumbersGame.py
+++ src/NumbersGame.py
@@ -1,7 +1,7 @@
 # Numbers game simulation
 
 import random
-from StringFormatting import format_results, find_possible_solutions
+from src.StringFormatting import format_results, find_possible_solutions
 
 digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 number_instance = []
